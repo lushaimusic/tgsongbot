4e6508c32f17ff1fbf360d6de7368aae33bdf534
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-05-23 17:38:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 import os
 API_ID = int(os.getenv("API_ID", "1234"))
-API_HASH = os.getenv("API_HASH" "5971f0a5ca31593e967cc69245abe1f1")
-BOT_TOKEN = os.getenv("BOT_TOKEN" "1891851003:AAGOg2fRzxHqBuCnZTNTT-XmOt4N3BmRyH4")
+API_HASH = os.getenv("API_HASH", "5971f0a5ca31593e967cc69245abe1f1")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "1891851003:AAGOg2fRzxHqBuCnZTNTT-XmOt4N3BmRyH4")
